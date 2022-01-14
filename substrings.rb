3d0a21a76e1bd907345edfed17139f630fbc97dd
Nicholas Leo <n.b.leo@protonmail.com>
2022-01-14 10:53:38 -0600

    Added support for non-matching dictionary items.

--- substrings.rb
+++ substrings.rb
@@ -1,11 +1,13 @@
 test_string = 'That dude got the basketball into the basketball basket!'
-test_dictionary = ['dude', 'basketball', 'basket', 'ball']
+test_dictionary = ['dude', 'basketball', 'basket', 'ball', 'finkelstein']
 
 def substrings(string, dictionary)
     split_string = string.downcase.split(' ')
     dictionary_match_counts = dictionary.reduce(Hash.new(0)) do |accumulator, word|
         match_count = split_string.filter_map {|string| true if string.include?(word)}.compact.length
-        accumulator[word] += match_count
+        unless match_count == 0
+            accumulator[word] += match_count
+        end
         accumulator
     end
     dictionary_match_counts
